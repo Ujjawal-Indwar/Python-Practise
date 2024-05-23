83a8d4806615749f3f36d0a8fa5fe3c5e24b98d5
unknown <ujjawal217@gmail.com>
2024-05-23 23:54:29 +0530

    Code added

---
+++ Pract6/HeatTailToss.py
@@ -0,0 +1,8 @@
+import random
+
+val = random.randint(0,1)
+
+if(val == 1):
+    print("Heads")
+else:
+    print("Tails")
---
+++ Pract6/Rondomcheck.py
@@ -0,0 +1,16 @@
+import random
+import math
+import pimodule
+
+rand_int = random.randint(0,6)
+
+rand_float = math.trunc(random.random()*10)
+#0-4.99999
+rand_fl = random.random()*5
+
+rand_pi = pimodule.pi
+
+print(rand_int)
+print(rand_float)
+print(rand_pi)
+print(rand_fl)
