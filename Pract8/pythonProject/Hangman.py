002f083b44de6b4df6b182b70dfe1b513a94e01d
unknown <ujjawal217@gmail.com>
2024-07-01 23:48:54 +0530

    Code added

--- Pract8/pythonProject/Hangman.py
+++ Pract8/pythonProject/Hangman.py
@@ -8,9 +8,47 @@ with open("MyFile.txt", "r") as file:
     word = random.choice(words)
 
     # print random string
-word_length = len(word)-1
+word_length = len(word)
 gap = random.randint(0,word_length)
+print(word_length)
 print(gap)
-for n in range(0,word_length-1):
-    print(n)
+for n in range(0,word_length):
+    print(end='_ ')
+
+print()
 print(word)
+originalLetters = list(word)
+
+
+#guess = ["_"]*word_length
+guess = []
+for _ in range(word_length):
+    guess +="_"
+
+print(guess)
+print(originalLetters)
+
+end_of_game = False
+
+while not end_of_game:
+    guessLetter = input(f'Guess an alphabet in this {word_length} letter word \n').lower()
+    for pos in range(word_length):
+        letter = originalLetters[pos]
+        if (letter == guessLetter):
+            guess[pos] = letter
+
+    print(guess)
+
+    if "_" not in guess:
+        end_of_game = True
+        print("You win")
+
+# for i ,letter in enumerate(originalLetters):
+#     if guessLetter==letter:
+#         guess[i]=letter
+#         print("yes")
+#
+#     else:
+#        print("no")
+
+
